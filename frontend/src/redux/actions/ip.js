c980864a02235096c61bd013a4c7ab5b87ce7cf6
Aniket  singh <aniketsingh16032@gmail.com>
2025-06-03 16:20:57 +0530

    Initial commit from personal project

---
+++ frontend/src/redux/actions/ip.js
@@ -0,0 +1,13 @@
+// Production ---->
+// export const BASE_URL = 'https://bookb.the-algo.com/api/v1';
+// export const BASE_SOCKET_URL = 'https://bookb.the-algo.com';
+
+// // Local ---->
+// export const BASE_URL = 'http://localhost:9191/api/v1';
+// export const BASE_SOCKET_URL = 'http://localhost:9191';
+
+
+// // Staging ---->
+export const BASE_URL = 'http://159.65.37.101:9191/api/v1';
+export const BASE_SOCKET_URL = 'http://159.65.37.101:9191';
+export const stripePublicKey = 'pk_test_HLKSK1hyuVsxEIZxIsEivNEj00RsqCdrBq'
